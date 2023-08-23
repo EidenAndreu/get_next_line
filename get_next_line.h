4b922b791457af3837043d98bb3cb739a46e3007
EidenAndreu <107546516+EidenAndreu@users.noreply.github.com>
2023-08-23 20:50:31 +0200

    Librerías añadidas

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ereinald <ereinald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 13:13:49 by ereinald          #+#    #+#             */
-/*   Updated: 2023/08/23 13:04:42 by ereinald         ###   ########.fr       */
+/*   Updated: 2023/08/23 20:49:05 by ereinald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 
 # include <stdlib.h>
 # include <stdio.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #define BUFFER_SIZE 42
 # endif
 
-size_t	ft_strlen(char *s);
+size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_get_line(char *pending);
 char	*ft_pending_trimmed(char *pending);
 char	*get_next_line(int fd);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: ereinald <ereinald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 13:14:00 by ereinald          #+#    #+#             */
-/*   Updated: 2023/08/23 13:11:07 by ereinald         ###   ########.fr       */
+/*   Updated: 2023/08/23 20:29:49 by ereinald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,47 +17,57 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (*s++ != 0)
-		i++;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;;
 	return (i);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != (char)c && *s != 0)
-		s++;
-	if (*s == (char)c)
-		return ((char *)s);
-	return (NULL);
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	int		i;
-	int		len1;
-	int		len2;
+	size_t	i;
+	size_t	j;
 	char	*str;
 
-	if (s1 && s2)
+	if (!s1)
 	{
-		len1 = ft_strlen(s1);
-		len2 = ft_strlen(s2);
-		str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-		if (str == NULL)
-			return (NULL);
-		i = -1;
-		while (s1[++i])
-			str[i] = s1[i];
-		i = -1;
-		while (s2[++i])
-		{
-			str[len1] = s2[i];
-			len1++;
-		}
-		str[len1] = '\0';
-		return (str);
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
 	}
-	return (NULL);
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }
 
 char	*ft_get_line(char *pending)
